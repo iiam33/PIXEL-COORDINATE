0ccd40bdab1641d03b0482e739d1d0e01fd6aa92
iiam33 <alvinyeoh0318@gmail.com>
2022-08-11 12:09:16 +0800

    fixed api

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import os
-from urllib import response
 from flask import Flask, render_template, request, jsonify, send_from_directory, redirect, url_for
 from werkzeug.utils import secure_filename
 from PIL import Image
 import requests
+from osgeo import gdal
 
 UPLOAD_FOLDER = 'static/img'
 BASE = 'http://127.0.0.1:5000'
 
 app = Flask(__name__)
-app.secret_key = "secret key"
+app.secret_key = 'secret key'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def convert_file(file_name):
-   name = '.' in file_name and file_name.split('.')[0].lower()
-   new_file_name = name + ".png"
+def convert_to_png(file_path):
+   f_name = file_path.replace(UPLOAD_FOLDER, "")
+   f_name = f_name.replace("\\", "")
+   name = '.' in f_name and f_name.split('.')[0].lower()
+   new_file_name = name + '.png'
 
-   processed_img = Image.open(file_name)
+   processed_img = Image.open(file_path)
    processed_img.thumbnail(processed_img.size)
-   processed_img.save(f"{UPLOAD_FOLDER}/{new_file_name}", "PNG", quality=100)
+   processed_img.save(f'{UPLOAD_FOLDER}/{new_file_name}', 'PNG', quality=100)
    return f"{UPLOAD_FOLDER}/{new_file_name}"
+
+def convert_to_tiff(file_path):
+   return
 
 @app.route('/', methods=['GET','POST'])
 def init():
@@ -30,18 +35,28 @@ def upload_file():
    if request.method == 'POST':
       uploaded_file = request.files['file']
       file_name = secure_filename(uploaded_file.filename)
-      uploaded_file.save(secure_filename(uploaded_file.filename))
-      # uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
-      processed_img = convert_file(file_name)
+      uploaded_file.save(os.path.join(UPLOAD_FOLDER, file_name))
+      processed_img = convert_to_png(os.path.join(UPLOAD_FOLDER, file_name))
       response = jsonify({'file-path': processed_img, 'message' : 'File successfully uploaded', 'status': 201})
       response.status_code = 201
-      return redirect(url_for('image', file = processed_img))
-   return response
+      # r = requests.post(BASE + '/image/' + processed_img)
+      # return redirect(url_for('image', file = processed_img))
+      return response
 
 @app.route('/image', methods = ['GET'])
 def image():
-   file = request.args.get('file')
+   res = requests.get(BASE + '/upload-file')
+   file = res.json().get('file-path')
+   # file = request.args.get('file-name')
    return render_template("image.html", image = file)
+
+# @app.route('/get-coordinate', methods = ['GET'])
+# def get_coordinate():
+#    name = "Block1994067ha_Orthomosaic_export_WedNov10094326109252"
+#    f_name = name + ".tif"
+#    file_path = os.path.join(UPLOAD_FOLDER, f_name)
+#    response = jsonify({'file-name': name, 'pixels' : {'x': 1000, 'y': 2000}, 'status': 201})
+#    return response
    
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0', port=5000)
